e992c8906c16a8ff4f0724b255ea8755b9ac68cd
RachelRYuan <yuanrui1580@gmail.com>
2024-05-18 12:13:46 -0500

    Initialize project using Create React App

---
+++ web/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache
+})
+
+function App(){
+  return (
+  <ApolloProvider client={client}> 
+    <div>test</div>
+  </ApolloProvider>
+    )
+}
+
+export default App;
