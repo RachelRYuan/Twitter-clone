69845274dcc3e2774e0be0d954c4ef9b22e66430
RachelRYuan <yuanrui1580@gmail.com>
2024-05-20 17:10:27 -0500

    React Routing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,19 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import Users from './components/Users';
+import Landing from './components/Landing'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+
 const client = new ApolloClient({
   uri: "http://localhost:4000",
   cache: new InMemoryCache
 })
 
 function App(){
-  return (
+  return (  
   <ApolloProvider client={client}> 
-    <div>
-      <Users />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="/landing" element={<Landing />} />
+      </Routes>
+    </Router>
   </ApolloProvider>
     )
-}
+} 
 
 export default App;
